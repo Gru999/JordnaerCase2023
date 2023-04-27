a968c409012b39dba47ef808f03f10a0babf1ef1
Daniel Hass <dani67h0@edu.zealand.dk>
2023-04-27 10:38:06 +0200

    Shift page

---
+++ JordnærCase2023/Pages/Shift/UpdateShift.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JordnærCase2023.Pages.Shift
+{
+    public class UpdateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
