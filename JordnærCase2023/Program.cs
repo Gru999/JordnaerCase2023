06bca76c7c28411d557eae2dc3162bfcd91adb78
Frederik Baun Grubak <fred00s7@edu.zealand.dk>
2023-05-02 11:25:59 +0200

    Comment

--- JordnærCase2023/Program.cs
+++ JordnærCase2023/Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddTransient<IEventService, EventService>();
+//builder.Services.AddTransient<IEventService, EventService>();
 
 // For user login
 builder.Services.AddSession();
