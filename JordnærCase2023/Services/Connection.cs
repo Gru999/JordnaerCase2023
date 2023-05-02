2955e4208e422ed6c54ef75c04955e5487be36bc
Daniel Hass <dani67h0@edu.zealand.dk>
2023-05-02 11:21:49 +0200

    ShiftCreate

---
+++ JordnærCase2023/Services/Connection.cs
@@ -0,0 +1,18 @@
+﻿namespace JordnærCase2023.Services
+{
+    public class Connection
+    {
+        protected string connectionString;
+        public IConfiguration Configuration { get; set; }
+        public Connection(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+        }
+        public Connection(string connectionString)
+        {
+            Configuration = null;
+            this.connectionString = connectionString;
+        }
+    }
+}
