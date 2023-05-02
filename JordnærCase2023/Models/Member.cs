df7b071cb30ca5aa5eece3784604ab4d42101096
hopefulbastard <miloskousgaard@gmail.com>
2023-05-02 11:28:20 +0200

    Member model

---
+++ JordnærCase2023/Models/Member.cs
@@ -0,0 +1,34 @@
+﻿namespace JordnaerMedlemmer.Models
+{
+    public class Member
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Phone { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool SanitationCourse { get; set; }
+        public bool Admin { get; set; }
+
+        public Member()
+        {
+
+        }
+
+        public Member(int id, string name, int phone, string email, string password, bool sanitationcourse, bool admin)
+        {
+            Id = id;
+            Name = name;
+            Phone = phone;
+            Email = email;
+            Password = password;
+            SanitationCourse = sanitationcourse;
+            Admin = admin;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
