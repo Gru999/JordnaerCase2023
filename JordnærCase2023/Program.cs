ee5ee596038c422ed12d9811dde9137d4e365058
Victor Jensen <Nexsysisa@gmail.com>
2023-05-03 14:23:45 +0100

    Item

--- JordnærCase2023/Program.cs
+++ JordnærCase2023/Program.cs
@@ -1,9 +1,18 @@
+﻿using JordnærCase2023.Interfaces;
+using JordnærCase2023.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+
+builder.Services.AddTransient<IItemService, ItemService>();
 var app = builder.Build();
+
+
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
---
+++ JordnærCase2023/Services/Connection.cs
@@ -0,0 +1,15 @@
+﻿namespace JordnærCase2023.Services
+{
+    public abstract class Connection
+    {
+        protected String connectionString;
+        public IConfiguration Configuration { get; }
+
+        public Connection(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+        }
+
+    }
+}
