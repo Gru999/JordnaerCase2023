c02b2fcc7962a811504ef465587b58c0fc1b27dc
Daniel Hass <dani67h0@edu.zealand.dk>
2023-05-25 09:14:36 +0200

    123

---
+++ JordnærCase2023/Pages/Shifts/JoinShift.cshtml.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JordnærCase2023.Pages.Shifts
+{
+    public class JoinShiftModel : PageModel
+    {
+        public void OnGet(int shiftId)
+        {
+
+        }
+        public void OnPost()
+        {
+
+        }
+    }
+}
